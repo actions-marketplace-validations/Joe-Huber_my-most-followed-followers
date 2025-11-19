7dd89e0ad771fc1dd9810193fd301a24056710f3
JoeH2025 <joseph.robert.huber@gmail.com>
2025-11-19 14:16:55 -0600

    sort followers

--- scrape_followers.py
+++ scrape_followers.py
@@ -1,7 +1,11 @@
+from github_user import GithubUser
+
 def scrape_all_followers():
     # code here
-    return 0
+    return []
 def scrape_curr_page():
     return 0
 def get_most_followed(num):
+    all_followers = scrape_all_followers()
+    all_followers.sort(key=lambda user: user.followers, reverse=True)
     return 0
