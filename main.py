c6c03401f925d196ed73144682eb0f6f226d219e
JoeH2025 <joseph.robert.huber@gmail.com>
2025-11-19 16:19:41 -0600

    top number

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from scrape_followers import get_most_followed
-github_username = "solidsnack"
+github_username = "Joe-Huber"
 github_followers_link = f"https://github.com/{github_username}?tab=followers"
+top_number = 5
 
 if __name__ == "__main__":
-    most_followed = get_most_followed(github_followers_link, 3)
+    most_followed = get_most_followed(github_followers_link, top_number)
     for user in most_followed:
         print(user)
