77f4e86d4d6d58865c167740a61d7c686ab3ed7c
JoeH2025 <joseph.robert.huber@gmail.com>
2025-11-20 14:08:10 -0600

    include when update occurs as a part of the main thing

--- README.md
+++ README.md
@@ -7,16 +7,6 @@ To get started, simply change the variables in `main.py` to your username and ho
 
 ### My Most Followed Followers
 <!-- FOLLOWERS_LIST_START -->
-| Profile | Name | Followers |
-|---|---|---|
-| <img src='https://avatars.githubusercontent.com/u/44704454?v=4' width='30' height='30'> | [AwesomeCoder412412](https://github.com/AwesomeCoder412412) | 9 |
-| <img src='https://avatars.githubusercontent.com/u/202472355?v=4' width='30' height='30'> | [macnmath](https://github.com/macnmath) | 2 |
-| <img src='https://avatars.githubusercontent.com/u/148095806?v=4' width='30' height='30'> | [KadenXu5001](https://github.com/KadenXu5001) | 1 |
-
 <!-- FOLLOWERS_LIST_END -->
 
 Psst, if you follow me you can show up on here! ^-^
-
-<!-- LAST_UPDATED_START -->
-*Last updated: 2025-11-20 00:52:46 UTC*
-<!-- LAST_UPDATED_END -->
--- update_readme.py
+++ update_readme.py
@@ -1,46 +1,39 @@
 from datetime import datetime
-from scrape_followers import get_most_followed
+from scrape_followers import get_most_followed, driver # Import driver
 from main import github_followers_link
 
 if __name__ == "__main__":
-    most_followed = get_most_followed(github_followers_link, 5)  # Get top 5
+    try:
+        # Scrape most followed users
+        most_followed = get_most_followed(github_followers_link, 5)  # Get top 5
 
-    with open("README.md", "r") as f:
-        readme_content = f.read()
+        with open("README.md", "r") as f:
+            readme_content = f.read()
 
-    # Create the markdown table
-    table = "| Profile | Name | Followers |\n|---|---|---|\n"
-    for user in most_followed:
-        table += f"| <img src='{user.profile_image_link}' width='30' height='30'> | [{user.name}]({user.link}) | {user.followers} |\n"
-
-    # Use placeholders to insert the table
-    followers_start_placeholder = "<!-- FOLLOWERS_LIST_START -->"
-    followers_end_placeholder = "<!-- FOLLOWERS_LIST_END -->"
-    
-    start_index = readme_content.find(followers_start_placeholder)
-    end_index = readme_content.find(followers_end_placeholder)
-
-    if start_index != -1 and end_index != -1:
-        readme_content = (
-            readme_content[:start_index + len(followers_start_placeholder)] +
-            "\n" + table + "\n" +
-            readme_content[end_index:]
-        )
-
-    # Update the "Last updated" timestamp
-    last_updated_placeholder_start = "<!-- LAST_UPDATED_START -->"
-    last_updated_placeholder_end = "<!-- LAST_UPDATED_END -->"
-    
-    start_index = readme_content.find(last_updated_placeholder_start)
-    end_index = readme_content.find(last_updated_placeholder_end)
-    
-    if start_index != -1 and end_index != -1:
+        # Create the markdown table and add the timestamp
         now_utc = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-        readme_content = (
-            readme_content[:start_index + len(last_updated_placeholder_start)] +
-            f"\n*Last updated: {now_utc} UTC*\n" +
-            readme_content[end_index:]
-        )
+        table = "| Profile | Name | Followers |\n|---|---|---|\n"
+        for user in most_followed:
+            table += f"| <img src='{user.profile_image_link}' width='30' height='30'> | [{user.name}]({user.link}) | {user.followers} |\n"
+        table += f"\n*Last updated: {now_utc} UTC*"
 
-    with open("README.md", "w") as f:
-        f.write(readme_content)
+        # Use placeholders to insert the table
+        followers_start_placeholder = "<!-- FOLLOWERS_LIST_START -->"
+        followers_end_placeholder = "<!-- FOLLOWERS_LIST_END -->"
+        
+        start_index = readme_content.find(followers_start_placeholder)
+        end_index = readme_content.find(followers_end_placeholder)
+
+        if start_index != -1 and end_index != -1:
+            readme_content = (
+                readme_content[:start_index + len(followers_start_placeholder)] +
+                "\n" + table + "\n" +
+                readme_content[end_index:]
+            )
+
+        with open("README.md", "w") as f:
+            f.write(readme_content)
+            
+    finally:
+        if driver: # Ensure driver exists before quitting
+            driver.quit()
