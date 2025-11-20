017c1759d8bf6e4ae2904c89f331fb620cb51b20
JoeH2025 <joseph.robert.huber@gmail.com>
2025-11-20 15:17:19 -0600

    add flags for container enviornment

--- scrape_followers.py
+++ scrape_followers.py
@@ -90,6 +90,9 @@ def setup(link):
     global driver
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=options)
     driver.get(link)
     driver.implicitly_wait(1)
